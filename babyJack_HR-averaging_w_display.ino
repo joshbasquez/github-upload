4a12e9c1121f1e618a35b91b2d966473386d0919
Josh Basquez <jobasque@microsoft.com>
2020-08-17 10:55:45 -0500

    initial commit - moving project

---
+++ babyJack_HR-averaging_w_display.ino
@@ -0,0 +1,230 @@
+/*
+  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 2nd, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  This is a demo to show the reading of heart rate or beats per minute (BPM) using
+  a Penpheral Beat Amplitude (PBA) algorithm.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening
+  device. Humans are generally bad at applying constant pressure to a thing. When you
+  press your finger against the sensor it varies enough to cause the blood in your
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 10; //Increase this for more averaging.
+long rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+long lastNoBeat = 0; 
+
+int bpmValid = 0;
+
+float beatsPerMinute;
+int beatAvg;
+int i = 0;
+
+static const unsigned char PROGMEM logo2_bmp[] =
+{ 0x03, 0xC0, 0xF0, 0x06, 0x71, 0x8C, 0x0C, 0x1B, 0x06, 0x18, 0x0E, 0x02, 0x10, 0x0C, 0x03, 0x10,              //Logo2 and Logo3 are two bmp pictures that display on the OLED if called
+0x04, 0x01, 0x10, 0x04, 0x01, 0x10, 0x40, 0x01, 0x10, 0x40, 0x01, 0x10, 0xC0, 0x03, 0x08, 0x88,
+0x02, 0x08, 0xB8, 0x04, 0xFF, 0x37, 0x08, 0x01, 0x30, 0x18, 0x01, 0x90, 0x30, 0x00, 0xC0, 0x60,
+0x00, 0x60, 0xC0, 0x00, 0x31, 0x80, 0x00, 0x1B, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x04, 0x00,  };
+
+static const unsigned char PROGMEM logo3_bmp[] =
+{ 0x01, 0xF0, 0x0F, 0x80, 0x06, 0x1C, 0x38, 0x60, 0x18, 0x06, 0x60, 0x18, 0x10, 0x01, 0x80, 0x08,
+0x20, 0x01, 0x80, 0x04, 0x40, 0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x02, 0xC0, 0x00, 0x08, 0x03,
+0x80, 0x00, 0x08, 0x01, 0x80, 0x00, 0x18, 0x01, 0x80, 0x00, 0x1C, 0x01, 0x80, 0x00, 0x14, 0x00,
+0x80, 0x00, 0x14, 0x00, 0x80, 0x00, 0x14, 0x00, 0x40, 0x10, 0x12, 0x00, 0x40, 0x10, 0x12, 0x00,
+0x7E, 0x1F, 0x23, 0xFE, 0x03, 0x31, 0xA0, 0x04, 0x01, 0xA0, 0xA0, 0x0C, 0x00, 0xA0, 0xA0, 0x08,
+0x00, 0x60, 0xE0, 0x10, 0x00, 0x20, 0x60, 0x20, 0x06, 0x00, 0x40, 0x60, 0x03, 0x00, 0x40, 0xC0,
+0x01, 0x80, 0x01, 0x80, 0x00, 0xC0, 0x03, 0x00, 0x00, 0x60, 0x06, 0x00, 0x00, 0x30, 0x0C, 0x00,
+0x00, 0x08, 0x10, 0x00, 0x00, 0x06, 0x60, 0x00, 0x00, 0x03, 0xC0, 0x00, 0x00, 0x01, 0x80, 0x00  };
+
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
+
+/////////////// ADDED FOR DISPLAY TEST IN VOID SETUP
+
+// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+Serial.println(F("SSD1306 allocation inprogress..."));
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  //display.display();
+  //delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+  display.display();
+} //close void setup
+
+void loop()
+{
+  long irValue = particleSensor.getIR();
+  long redValue = particleSensor.getRed();
+
+  if (irValue > 50000)
+    {
+    display.clearDisplay();                                   //Clear the display
+    display.drawBitmap(5, 5, logo2_bmp, 24, 21, WHITE);       //Draw the first bmp picture (little heart)
+    display.setTextSize(2);                                   //Near it display the average BPM you can display the BPM if you want
+    display.setTextColor(WHITE); 
+    display.setCursor(50,0);                
+    display.println("BPM");             
+    display.setCursor(50,18);                
+    display.println(beatAvg); 
+    display.display();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    tone(3,1000);                                        //And tone the buzzer for a 100ms you can reduce it it will be better
+    delay(100);
+    noTone(3);
+    
+    display.clearDisplay();                                //Clear the display
+    display.drawBitmap(0, 0, logo3_bmp, 32, 32, WHITE);    //Draw the second picture (bigger heart)
+    display.setTextSize(2);                                //And still displays the average BPM
+    display.setTextColor(WHITE);             
+    display.setCursor(50,0);                
+    display.println("BPM");             
+    display.setCursor(50,18);                
+    display.println(beatAvg); 
+    display.display();
+
+    //beatsPerMinute = 60 / (delta / 1000.0);
+
+    //if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    //{
+    //  rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+    //  rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+    //  beatAvg = 0;
+     
+     for (byte x = 0 ; x < (RATE_SIZE - 1) ; x++)
+     {
+      rates[x] = rates[x+1];
+     }
+     rates[9] = delta;
+
+     //calculate average bpm
+     beatAvg = 0;
+     for (byte x = 0 ; x < (RATE_SIZE) ; x++)
+     {
+      beatAvg += rates[x];
+      }
+     beatAvg /= RATE_SIZE;
+     beatAvg = (60000 / beatAvg);
+ 
+     //   beatAvg += rates[x];
+    
+  Serial.print("We detected a beat. Time since last beat: ");
+  Serial.print(delta);
+  Serial.print(" BPM: ");
+  Serial.print(60000 / delta);
+  //Serial.print(" arr vals: ");
+  //Serial.print(rates[0]);
+  //Serial.print(",");
+  //Serial.print(rates[1]);
+  //Serial.print(",");
+  //Serial.print(rates[2]);
+  //Serial.print(",");
+  //Serial.print(rates[3]);
+  Serial.print(",");
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(",");
+  Serial.print("RED=");
+  Serial.print(redValue);  
+  Serial.print(",");
+  Serial.print("BPM AVG=");
+  Serial.print(beatAvg);  
+  Serial.println();
+  
+  } //close if beat detected
+
+  //Serial.print("IR=");
+  //Serial.print(irValue);
+  //Serial.print(", BPM=");
+  //Serial.print(beatsPerMinute);
+  //Serial.print(", Avg BPM=");
+  //Serial.print(beatAvg);
+  
+    } //IF IRVALUE OVER 50000
+  if (irValue < 50000)
+    {
+      
+      bpmValid = 0;
+      if ((millis() - lastNoBeat) > 1000)
+      {
+      lastNoBeat = millis();
+      Serial.println(" No finger?");
+      display.clearDisplay();
+      display.setTextSize(1);                    
+      display.setTextColor(WHITE);             
+      display.setCursor(30,5);                
+      display.println("Please Place "); 
+      display.setCursor(30,15);
+      display.println("your finger ");  
+      display.display();
+      }
+      
+    }
+
+}
